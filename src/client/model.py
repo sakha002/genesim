f0a3c5831824a08e18f1d0bbb3d8ee5da8ff1766
sakhavanhejazi <h.akhavanhejazi@shell.com>
2022-03-09 11:42:20 -0800

    add sample data

--- src/client/model.py
+++ src/client/model.py
@@ -1,9 +1,24 @@
+import json
+from google.protobuf.json_format import ParseDict, MessageToDict
 
 from genesys_pb2 import(
     GenModelRequest,
 ) 
 
+FILE_NAME = "./sample_inputs/simple_battery.json"
+
+def read_json_into_proto(file_name):
+    with open(file_name) as file:
+        input_dict = json.load(file)
+
+    request = ParseDict(
+        js_dict = input_dict,
+        message= GenModelRequest(),
+        ignore_unknown_fields=False
+    )
+    return request
+
 def instantiate_model():
-    model = GenModelRequest()
+    model = read_json_into_proto(FILE_NAME)
 
     return model
